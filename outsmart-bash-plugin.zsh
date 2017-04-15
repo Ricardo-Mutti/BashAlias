fa15ae1274490f7e63e7df33fb0843b3a4049018
Mutti <mutti.appsimples@gmail.com>
2017-04-15 17:03:07 -0300

    Folder Aliases

--- outsmart-bash-plugin.zsh
+++ outsmart-bash-plugin.zsh
@@ -1,2 +1,17 @@
 #outsmart.plugin.zsh
+
+#ALIAS
 alias cadeochinelo='echo ta aqui!'
+alias android='cd ~/AppsAndroid; ls'
+alias certs='cd ~/Certs; ls'
+alias ios='cd ~/AppsIOS; ls'
+alias listenPorts='netstat -atn | grep LISTEN'
+alias scripts='cd ~/Scripts; ls'
+alias servers='cd ~/Servers; ls'
+alias web='cd ~/Web; ls'
+
+#FUNCTIONS
+function cdls() { cd "$1"; ls ; }
+function updateBash() { cd ~/.oh-my-zsh/custom/; git pull; cd; }
+function pidOnPort() {sudo lsof -i :"$1";}
+function devServer() { ssh -i ~/Certs/DevelopmentEC2.pem ubuntu@52.72.229.94}
